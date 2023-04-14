7c57f6f375948fd0f1012da05f0a11da080b5cc7
Ivo Dias <igd753@gmail.com>
2023-04-14 11:44:04 -0300

    DelUser V1

---
+++ Identity API/Delete/deleteUser.ps1
@@ -0,0 +1,65 @@
+param (
+    $emailUsuario
+)
+
+function Check-VeracodeAPI {
+    param (
+        $retornoAPI
+    )
+    
+    # Filtra a resposta
+    $status = $retornoAPI.http_status
+    $mensagem = $retornoAPI.message
+    $codigoErro = $retornoAPI.http_code
+
+    if ($status -eq "Bad Request") {
+        Write-Host "Ocorreu um erro:"
+        Write-Host $mensagem
+        Write-Error $codigoErro
+        exit
+    } if ($status -eq "Unauthorized") {
+        Write-Host "Erro de autenticação:"
+        Write-Host $mensagem
+        Write-Host "Favor entrar em contato com o suporte"
+        Write-Error $codigoErro
+        exit
+    } else {
+        $validador = "OK"
+        return $validador
+    }
+}
+
+function Get-VeracodeUserID {
+    param (
+        $emailUsuario
+    )
+    $retornoAPI = http --auth-type=veracode_hmac GET "https://api.veracode.com/api/authn/v2/users?user_name=$emailUsuario" | ConvertFrom-Json
+    $validador = Check-VeracodeAPI $retornoAPI
+
+    if ($validador -eq "OK") {
+        $idUsuario = $retornoAPI._embedded.users.user_id
+        if ($idUsuario) {
+            return $idUsuario
+        } else {
+            Write-Error "Não foi localizado nenhum ID para: $emailUsuario"
+            exit
+        }
+        
+    } else {
+        Write-Error "Comportamento não esperado"
+        exit
+    }
+}
+
+try {
+    # Recebe o ID com base no nome
+    $idUsuario = Get-VeracodeUserID $emailUsuario
+
+    # Deleta o usuario
+    http --auth-type=veracode_hmac DELETE "https://api.veracode.com/api/authn/v2/users/$idUsuario"
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host "Erro no Powershell:"
+    Write-Host "$ErrorMessage"
+}
